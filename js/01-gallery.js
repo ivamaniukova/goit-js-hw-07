dbd872b5eae42895a9b830a394110a9879e86b70
ivamaniukova <139200268+ivamaniukova@users.noreply.github.com>
2023-11-14 20:37:06 -0800

    finished

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageList = document.querySelector(".gallery");
+
+imageList.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+imageList.addEventListener("click", handleClick);
+
+function createMarkup(array) {
+    return array.map(({ preview, original, description}) => 
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+    </li>`).join("");
+};
+
+
+
+function handleClick(event) {
+event.preventDefault()
+
+    if(event.target === event.currentTarget){
+        return;
+    }
+
+    const modalImg = event.target.closest(".gallery__link").href
+
+    const instance = basicLightbox.create(`<img class ="gallery__image" src="${modalImg}" alt="${modalImg.description}">`);
+
+    instance.show()
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageList = document.querySelector(".gallery");
+
+imageList.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+imageList.addEventListener("click", handleClick);
+
+
+
+function createMarkup(array) {
+    return array.map(({ preview, original, description}) => 
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`).join("");
+};
+
+
+
+function handleClick(event) {
+event.preventDefault()
+
+    if(event.target === event.currentTarget){
+        return galleryItems.next();
+    }
+    let lightbox = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250});
+    // const modalImg = event.target.closest(".gallery__link").href
+
+    // lightbox.on('show.simplelightbox', function () {
+    //     `<img class ="gallery__image" src="${modalImg.original}" alt="${modalImg.description}">`});
+
+}
